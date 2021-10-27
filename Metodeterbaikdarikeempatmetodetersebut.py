b8e169da376b465b0292ab0c875b0a66e7fe7586
AnjasMuhamadRafi <anjas5rafi45@gmail.com>
2021-10-27 21:43:50 +0700

    Praktikum1_Metnum

---
+++ Metodeterbaikdarikeempatmetodetersebut.py
@@ -0,0 +1,2 @@
+message = 'Metode Newton-Rapshon'
+print ("Updated String :-", message[:0] + 'Karena metode Newton-Raphson konvergen paling cepat dan paling akurat dibandingkan metode Secant dan metode Bisection')
